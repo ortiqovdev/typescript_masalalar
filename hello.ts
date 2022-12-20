33e128023a28ac4f8a8c0c0944e09f98f6489040
ortiqovdev <akmalbekortiqov7976@gmail.com>
2022-12-20 09:44:24 +0500

    ts masalalar 18gacha

---
+++ hello.ts
@@ -0,0 +1,104 @@
+//masala 1
+// let a: number= 20
+// let p:number = 4*a
+// console.log(p)
+
+
+//masala 2
+// let a: number=4
+// console.log(a*a);
+
+//masala 3
+// let a:number=5
+// let b:number=10
+// let s:number=a*b
+// let p:number= a*b*2
+// console.log(`Yuzasi: ${s} peremetri : ${p} ` );
+
+
+//masala 4
+// let d:number = 5
+// console.log(Math.PI * d);
+
+//masala 5
+// let a:number = 5
+// let b:number = 6
+// console.log(`hajmi ${a**3}  sirti ${b*a*a}`)
+
+// masala 6
+// let a:number =10
+// let b:number =15
+// let c:number =20
+// let v:number = a*b*c
+// let d:number = a*b
+// let g:number=b*c
+// let f:number=a*c
+// let t:number=d+g+f
+// console.log(`${v} ${t*2} `);
+
+// masala 7
+// let r:number= 5
+// let l:number= r*2
+// let s:number= r*r
+// console.log(`uzunligi: ${l*Math.PI}  yuzasi: ${s*Math.PI}`);
+
+// masala 8
+// let a:number=10
+// let b:number=20
+// let s:number=a+b
+// console.log(`Orta arifmetigi: ${s/2}`);
+
+// masala 9
+// let a:number =4
+// let b:number = 4
+// console.log(Math.sqrt(a*b));
+
+// masala 10
+// let a:number=5
+// let b:number=10
+// console.log(`yigindisi : ${a+b} kopaytmasi: ${a*b} kavadrati a ${a*a } kavadrati b ${b*b }`);
+
+// masala 11
+//  let a:number=5
+//  let b:number=10
+//  console.log(`yigindisi : ${a+b} kopaytmasi: ${a*b} moduli : ${Math.abs(a+b) }`);
+
+// masala 12
+// let a:number=10
+// let b:number=20
+// let c:number=a*a
+// let d:number=b*b
+// let r:number= a+b
+// console.log(`Gipotenuzasi:  ${Math.sqrt(c+d)} Peremetri:  ${Math.sqrt(c+d)+r}`);
+
+// masala  13
+// let r1:number = 10
+// let r2:number = 5
+// let a:number = r1-r2
+// console.log(`S1=${(Math.PI)*r1} S2=${Math.PI*r2} S3=${Math.PI*a} `);
+
+// masala 14
+// let l:number= 10
+// let r:number=l/2*Math.PI
+// console.log(`${Math.PI*r*r}`);
+
+// masala 15
+// let s:number= 10
+// let r:number=Math.sqrt(s/Math.PI) 
+// console.log(`${r*2}`);
+
+// masala 16
+// let x1:number=15
+// let x2:number=20
+// console.log(x2-x1);
+
+// masala 17
+// let a:number=5
+// let b:number=10
+// let c:number=15
+// let p:number=a+b+c
+// let d:number=a+c
+// let f:number=b+c
+// console.log(`Uzunligi ${p} A va C niki:${d} B va C niki: ${f}`);
+
+export{}
